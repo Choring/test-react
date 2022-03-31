311cad3fc62d4c0588f2b26b04174b07945d2461
Choring <skyhonor201@gmail.com>
2022-03-31 15:14:37 +0900

    test_commit

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -5,6 +5,7 @@ const MyComponent = props => {
             안녕하세요, 제 이름은{props.name}입니다.<br/>
             children 값은 {props.children}
             입니다. hahaha
+            hahahaha
         </div>
     );
 };
